1fa3d7f49d7cccdfbcda2661f9de23e14abf4920
Deepak <raj.deeepak@gmail.com>
2018-09-15 22:44:45 +0530

    [+] Url routing

--- pages/views.py
+++ pages/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home_view(*args, **kargs):
+def home_view(request, *args, **kargs):
+    print(args, kargs)
+    print(request.user)
     return HttpResponse("<h1> This is home view page</h1>")
+
+def contact_view(*args, **kargs):
+    return HttpResponse("<h1> This is Contact view page</h1>")
+
+def about_view(*args, **kargs):
+    return HttpResponse("<h1> This is About view page</h1>")
--- tryDjango/urls.py
+++ tryDjango/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view
+from pages.views import home_view, contact_view, about_view
 
 urlpatterns = [
+    
+    path('contact/', contact_view, name="Contact"),
+    path('about/', about_view, name="About"),
     path('', home_view, name="home"),
     path('admin/', admin.site.urls),
 ]
